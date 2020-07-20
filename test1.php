2de4655abcdccfd79015819b319504e6781f2768
IAEmam <islamemam95@yahoo.com>
2020-07-21 01:36:14 +0200

    fix conflict

--- test1.php
+++ test1.php
@@ -1,10 +1,7 @@
 <?php
 echo "islam merge";
 echo "islam merge2222";
-<<<<<<< HEAD
 echo "islam merge3000";
-=======
->>>>>>> parent of 249deba... merge1000
+echo "islam merge2000";
 echo "test15";
-
 ?>
